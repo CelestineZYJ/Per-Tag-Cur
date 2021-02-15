c5cd382c8e34d8a32aa994465e4ebd26cc3a7e06
Celestine <864409320@qq.com>
2021-02-15 16:33:40 +0800

    Add files via upload

--- modules/neumf.py
+++ modules/neumf.py
@@ -25,21 +25,36 @@ class Neumf(torch.nn.Module):
     def forward(self, user_indices, item_indices):
         # print(user_indices.size())  # torch.Size([128=batch_size])
         # print(user_indices)
+        # print(item_indices)
         user_embedding_mlp = self.embedding_user_mlp(user_indices)
-        print(user_embedding_mlp.size())
-        print(user_embedding_mlp)
+        # print(user_embedding_mlp)
+        # print(user_embedding_mlp.size()) # torch.Size([128=batch_size, 8])
         item_embedding_mlp = self.embedding_item_mlp(item_indices)
+        # print(item_embedding_mlp)
+        # print(item_embedding_mlp.size())
         user_embedding_mf = self.embedding_user_mf(user_indices)
+        # print(user_embedding_mf)
+        # print(user_embedding_mf.size())
         item_embedding_mf = self.embedding_item_mf(item_indices)
-
+        # print(item_embedding_mf)
+        # print(item_embedding_mf.size())
         mlp_vector = torch.cat([user_embedding_mlp, item_embedding_mlp], dim=-1)
+        # print(mlp_vector)
+        # print(mlp_vector.size()) # torch.Size([128=batch_size, 8+8=16])
         mf_vector = torch.mul(user_embedding_mf, item_embedding_mf)
+        # print(mf_vector)
+        # print(mf_vector.size()) # torch.Size([128=batch_size, 8=8])
 
         for idx, _ in enumerate(range(len(self.fc_layers))):
             mlp_vector = self.fc_layers[idx](mlp_vector)
             mlp_vector = torch.nn.ReLU()(mlp_vector)
 
+        # print(mlp_vector)
+        # print(mlp_vector.size())
+
         vector = torch.cat([mlp_vector, mf_vector], dim=-1)
+        # print(vector)
+        # print(vector.size())
         # logits = self.affine_output(vector)
         # rating = self.logistic(logits)
         # return rating
